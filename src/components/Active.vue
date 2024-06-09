86fa471c2aaa5b6f35fad227ea5ff934b889ef07
Woody Ramey <woodyramey@icloud.com>
2024-06-09 15:00:16 -0500

    Add files via upload

--- src/components/Active.vue
+++ src/components/Active.vue
@@ -10,6 +10,7 @@ const filteredTakes = computed(() => {
   return takes.value.filter(item => new Date(item.deadline) >= today);
 });
 
+console.log(filteredTakes.value)
 // Function to format the date
 const formatDate = (date) => {
   const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
@@ -46,26 +47,43 @@ onUnmounted(unsubscribe);
 
 <template>
 
-  <section class="w-full grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 md:gap-6 xl:gap-8 text-[16px] text-[#E5E7EB] font-medium px-[2%] items-start ">
-    <div v-for="item in filteredTakes" :key="item.id"  class="w-full border rounded-lg border-green-700 md:hover:border-green-600 duration-200 md:skew-x-6 md:hover:skew-x-0 bg-[#19702713]">
+  <section class=" ">
+
+    <div v-if="filteredTakes.length > 0" class="w-full grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 md:gap-6 xl:gap-8 text-[16px] text-[#E5E7EB] font-medium px-[2%] items-start">
+
+      <div v-for="item in filteredTakes" :key="item.id"  class="w-full border rounded-lg border-green-700 md:hover:border-green-600 duration-200 md:skew-x-6 md:hover:skew-x-0 bg-[#19702713]">
+      
         <router-link :to="/Take/ + item.id" class="flex flex-col gap-4 bg-[#161313] p-3 rounded-lg border border-gray-700 md:skew-x-4 md:translate-x-[-10px] md:translate-y-[-10px] duration-300 md:hover:translate-x-0 md:hover:translate-y-0 ">
           <div class="flex gap-2 items-center">
+
             <img :src="item.uicon" alt="" srcset="" class="w-8 rounded-full">
+
             <div v-if="item.user" >
               <h2 class="text-sm">{{ item.user }}</h2>
             </div>
+
             <div class="text-sm" v-else>
               <h2 > {{ item.email.slice(0,10) }}</h2>
             </div>
             
-            
           </div>
+
           <h2 class="text-[19px]">{{ item.title }}</h2>
+
           <div class="text-gray-400">
             Until {{ formatDate(item.deadline) }}
           </div>
+          
         </router-link>
+        
+      </div>
+
     </div>
+
+    <div v-else class="text-white font-medium">
+          No take yet 😅
+    </div>
+
   </section>
 
 </template>
--- src/components/Past.vue
+++ src/components/Past.vue
@@ -38,21 +38,31 @@ onUnmounted(unsubscribe);
 
 <template>
 
-  <section  class="w-full grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-3 md:gap-6 xl:gap-8 text-[16px] text-[#E5E7EB] font-medium px-[2%] items-start ">
-    <div v-for="item in filteredTakes" :key="item.id"  class="w-full border rounded-lg border-red-700 md:hover:border-red-600 duration-200 md:skew-x-6 md:hover:skew-x-0 bg-[#7a1d1d10]">
-        <router-link :to="/Take/ + item.id" class="flex flex-col gap-4 bg-[#161313] p-3 rounded-lg border border-gray-700 md:skew-x-4 md:translate-x-[-10px] md:translate-y-[-10px] duration-300 hover:translate-x-0 hover:translate-y-0 ">
-          <div class="flex gap-2 items-center">
-            <img :src="item.uicon" alt="" srcset="" class="w-8 rounded-full">
-            <h2 class="text-sm">{{ item.user }}</h2>
-          </div>
-          <h2 class="text-[20px]">{{ item.title }}</h2>
-          <div class="flex justify-start">
-            <div class="bg-yellow-400 rounded-full text-black py-1 px-2 text-sm font-medium">
-                Expired
+  <section  class="">
+
+    <div v-if="filteredTakes.length > 0" class="w-full grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 md:gap-6 xl:gap-8 text-[16px] text-[#E5E7EB] font-medium px-[2%] items-start">
+
+      <div v-for="item in filteredTakes" :key="item.id"  class="w-full border rounded-lg border-red-700 md:hover:border-red-600 duration-200 md:skew-x-6 md:hover:skew-x-0 bg-[#7a1d1d10]">
+          <router-link :to="/Take/ + item.id" class="flex flex-col gap-4 bg-[#161313] p-3 rounded-lg border border-gray-700 md:skew-x-4 md:translate-x-[-10px] md:translate-y-[-10px] duration-300 hover:translate-x-0 hover:translate-y-0 ">
+            <div class="flex gap-2 items-center">
+              <img :src="item.uicon" alt="" srcset="" class="w-8 rounded-full">
+              <h2 class="text-sm">{{ item.user }}</h2>
             </div>
-          </div>
-        </router-link>
+            <h2 class="text-[20px]">{{ item.title }}</h2>
+            <div class="flex justify-start">
+              <div class="bg-yellow-400 rounded-full text-black py-1 px-2 text-sm font-medium">
+                  Expired
+              </div>
+            </div>
+          </router-link>
+      </div>
+
     </div>
+
+    <div v-else class="text-white font-medium">
+          No expired take yet 😅
+    </div>
+    
   </section>
 
 </template>
